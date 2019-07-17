afef348a7c8aaf9c85ac6c7f2f81c2581a853e09
Glenn <ggilmartin@overstock.com>
2019-07-17 12:31:14 +0100

    Fixed various tests to handle hashed password in db

--- nodes-api/lib/nodes_api_web/router.ex
+++ nodes-api/lib/nodes_api_web/router.ex
@@ -1,11 +1,12 @@
 defmodule NodesApiWeb.Router do
   use NodesApiWeb, :router
-
+  
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/api", NodesApiWeb do
     pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
   end
 end
---
+++ nodes-api/lib/nodes_api_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule NodesApiWeb.UserView do
+  use NodesApiWeb, :view
+  alias NodesApiWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      email: user.email,
+      is_active: user.is_active
+    }
+  end
+end
